b74d6d469a4fb35139eff9a540c14fda389c7b1b
TheBreckoning <97486201+thebreckoning@users.noreply.github.com>
2023-01-28 13:08:07 -0800

    Update WhaleFinder.py

--- WhaleFinder.py
+++ WhaleFinder.py
@@ -1,10 +1,11 @@
+#The purpose of this script is to help me clean up my hard drive by finding all the large files so I can review and delete anything I don't need.
 
 import os
 import csv
 import mimetypes
 
 # Set the threshold for file size (in bytes)
-threshold = 10 * 1024 * 1024 * 1024
+threshold = 5 * 1024 * 1024 * 1024
 
 # Create a folder to save the csv
 if not os.path.exists("Whales"):
@@ -13,10 +14,10 @@ if not os.path.exists("Whales"):
 # Open a CSV file for writing
 with open('Whales/Whales.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['File Type', 'File Size (GB)', 'File Name', 'File Location'])
+    writer.writerow(['File Name', 'File Size (GB)', 'File Type', 'File Location'])
 
-    # Walk through all the files and directories in the C drive
-    for dirpath, dirnames, filenames in os.walk('C:\\'):
+    # Walk through all the files and directories on the entire computer
+    for dirpath, dirnames, filenames in os.walk('/'):
         for filename in filenames:
             # Get the full file path
             file_path = os.path.join(dirpath, filename)
@@ -28,4 +29,4 @@ with open('Whales/Whales.csv', 'w', newline='') as csvfile:
                 # Get the file size in GB
                 file_size = os.path.getsize(file_path) / (1024 * 1024 * 1024)
                 # Write the file information to the CSV file
-                writer.writerow([file_type, file_size, filename, file_path])
+                writer.writerow([filename, file_size, file_type, file_path])
